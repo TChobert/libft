10ba17ab46db01f122ded09512538b09ce928343
Thomas Chobert <thomaschobert00@gmail.com>
2024-03-27 18:45:01 +0100

    27/03 ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,64 @@
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	uint8_t	*current;
+	uint8_t	value;
+	size_t	size;
+
+	current = s;
+	value = (uint8_t)c;
+	size = 0;
+	while (size < n)
+	{
+		current[size] = value;
+		++size;
+	}
+	return (s);
+}
+
+/*
+int main(void)
+{
+	char	array[] = "AAAAA";
+	char	array2[] = "AAAAA";
+	ft_memset(array + 2, 86, 3);
+	memset(array2 + 2, 86, 3);
+	printf("mine = %s\n", array);
+	printf("true = %s\n", array2);
+	return (EXIT_SUCCESS);
+}
+
+int	main(void)
+{
+	int	count = 0;
+	int	array[] = {1, 2, 3};
+	int	array2[] = {1, 2, 3};
+	int size = sizeof(int) * 3;
+	while (count < 3)
+	{
+		printf("%d", array[count]);
+		++count;
+	}
+	printf("\n");
+	ft_memset(array, 'A', size);
+	count = 0;
+	printf("mine =");
+	while (count < 3)
+	{
+		printf("%d", array[count]);
+		++count;
+	}
+	printf("\n");
+	memset(array2, 'A', size);
+	count = 0;
+	printf("true =");
+	while (count < 3)
+	{
+		printf("%d", array2[count]);
+		++count;
+	}
+	printf("\n");
+	return (EXIT_SUCCESS);
+}
+*/
--- libft.h
+++ libft.h
@@ -3,20 +3,23 @@
 
 // INCLUDES --------------------------------------------------------------------
 
+# include <stdint.h>
 # include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
 # include <stdbool.h>
 # include <ctype.h>
 # include <string.h>
+# include <stdlib.h>
+
+# include <stdio.h>
 
 // PROTOTYPES ------------------------------------------------------------------
 
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int	ft_isprint(int c);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
 size_t	ft_strlen(const char *s);
+void	*ft_memset(void *s, int c, size_t n);
 
 #endif
