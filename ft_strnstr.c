4a2f32d06e2d71ba21b99d87e849e2d24ac9b129
Thomas Chobert <thomaschobert00@gmail.com>
2024-04-25 13:54:37 +0200

    25/04 ft_strnstr, modif output

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,29 +3,36 @@
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	little_len;
+	char	*output;
 
+	output = (char *)(big);
 	little_len = ft_strlen(little);
 	if (little_len > len)
 		return (NULL);
 	if (*little == '\0')
-		return (char *)(big);
+	{
+		return (output);
+	}
 	while (*big != '\0' && len >= little_len)
 	{
-		if(ft_strncmp(big, little, len) == 0)
-			return (char *)(big);
+		if (ft_strncmp(big, little, len) == 0)
+		{
+			return (output);
+		}
 		++big;
 		--len;
 	}
 	return (NULL);
 }
-
+/*
 int	main(int ac, char **av)
 {
-	char	str1[] = "ABCDEF";
-	char	str2[] = "BC";
+	char	str1[] = "";
+	char	str2[] = "";
 	(void)ac;
 	(void)av;
-	printf("%s\n", ft_strnstr(str1, str2, 1));
-	printf("%p\n", ft_strnstr(str1, str2, 1));
+//	printf("%s\n", ft_strnstr(str1, str2, 3));
+	printf("%p\n", ft_strnstr(str1, str2, 3));
 	return (EXIT_SUCCESS);
 }
+*/
