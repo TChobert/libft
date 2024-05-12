57a0e0b77d5e41c574e0af42507d07c017ee1d54
Thomas Chobert <thomaschobert00@gmail.com>
2024-05-12 16:30:28 +0200

    12/05 - ft_strncmp, modifs CORRECTION

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -2,21 +2,20 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	const unsigned char	*u_s1 = (const unsigned char *)s1;
-	const unsigned char	*u_s2 = (const unsigned char *)s2;
 	size_t				i;
 
 	i = 0;
-	while ((u_s1[i] != '\0' || u_s2[i] != '\0') && i < n)
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
-		if (u_s1[i] != u_s2[i])
+		if (s1[i] != s2[i])
 		{
-			return (u_s1[i] - u_s2[i]);
+			return ((unsigned char )s1[i] - (unsigned char )s2[i]);
 		}
 		++i;
 	}
 	return (0);
 }
+
 /*
 int	main(int ac, char **av)
 {
