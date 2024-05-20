03694724e0a75e7df22baa276e26ecdfbe07c756
Thomas Chobert <tchobert@paul-f4Br8s3.clusters.42paris.fr>
2024-05-20 18:17:09 +0200

    20/05 - correction calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -5,15 +5,15 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*new_area;
 	size_t	total_size;
 
-	total_size = (nmemb * size);
-	new_area = (void *)malloc(total_size);
-	if (nmemb != 0 && total_size / nmemb != size)
+	if (nmemb != 0 && size > SIZE_MAX / nmemb)
 	{
 		return (NULL);
 	}
+	total_size = (nmemb * size);
+	new_area = (void *)malloc(total_size);
 	if (new_area != NULL)
 	{
-		ft_bzero(new_area, nmemb * size);
+		ft_bzero(new_area, total_size);
 	}
 	return (new_area);
 }
